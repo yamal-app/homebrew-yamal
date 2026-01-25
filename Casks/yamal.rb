760666b54f279a3c0f346dd9214b8a82b2deed7a
Pavel Gulbin <pgulbin@mts.ru>
2026-01-25 22:07:23 +0300

    init

---
+++ Casks/yamal.rb
@@ -0,0 +1,16 @@
+cask "yamal" do
+  version "2026.1.3"
+  sha256 "ad26a783bdcd15d9f814799cccb4bdeed172504a1d7425f6308d16c732bfc25e"
+
+  url "https://storage.mwsapis.ru/yamal/release/Yamal_#{version}.zip"
+
+  name "Yamal"
+  desc "Lightweight, fast, and beautiful Kubernetes client for macOS"
+  homepage "https://yamal.app"
+
+  app "Yamal.app"
+
+  uninstall trash: "~/Library/Preferences/org.yamal.Yamal.plist",
+            delete:  "/Applications/Yamal.app"
+
+end
