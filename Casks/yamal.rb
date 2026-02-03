0fd9eeeb4dcb78077d18a79915dd31d5b50e952a
Pavel Gulbin <pgulbin@mts.ru>
2026-02-04 00:35:43 +0300

    fix url

--- Casks/yamal.rb
+++ Casks/yamal.rb
@@ -2,7 +2,7 @@ cask "yamal" do
   version "2026.1.3"
   sha256 "ad26a783bdcd15d9f814799cccb4bdeed172504a1d7425f6308d16c732bfc25e"
 
-  url "https://storage.mwsapis.ru/yamal/release/Yamal_#{version}.zip"
+  url "https://yamal.app/download/Yamal_#{version}.zip"
 
   name "Yamal"
   desc "Lightweight, fast, and beautiful Kubernetes client for macOS"
